05e28cea6e760a2a3f36a6ca671a5b8c48b98cef
Benjamin Malik <56892822+bmalik1894@users.noreply.github.com>
2021-04-01 18:34:43 -0500

    Tortise -> Tortoise

--- server/app/actors/CanvasManager.scala
+++ server/app/actors/CanvasManager.scala
@@ -7,7 +7,7 @@ import scala.util.Random
 
 class CanvasManager extends Actor {
     import CanvasManager._
-    private var characterNames = Array("Badger", "Armadillo", "Dragonfly", "Camel", "Flamingo", "Tortise", "Ostritch", "Python", "Rat")
+    private var characterNames = Array("Badger", "Armadillo", "Dragonfly", "Camel", "Flamingo", "Tortoise", "Ostritch", "Python", "Rat")
     private var colors = Array("blue", "red", "orange", "black", "brown", "gray", "purple", "pink", "green")
     var users:Array[User] = Array()
     private val random = new Random
